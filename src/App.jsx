cceb7a9c6269608ed966819dc45e6acce822f9f2
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-09-23 20:04:11 +0530

    changed the background snipett

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,14 @@ function App() {
   return (
     <div className="antialiased">
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div
-          className="absolute top-0 z-[-2] h-screen
-       w-screen bg-neutral-950 
-       bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
-        />
+        <div className="relative h-full w-full bg-slate-950">
+          <div
+            className="absolute bottom-0 left-0 right-0 top-0
+             bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)]
+              bg-[size:14px_24px]
+       [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"
+          ></div>
+        </div>
       </div>
 
       {/* <div className="fixed top-0 w-full">
