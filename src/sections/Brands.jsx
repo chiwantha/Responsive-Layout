f800d33dcc25d20a10ed8354d693c7a7d5fbda1c
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-09-23 16:35:14 +0530

    fix framer motion x asix toggle error ( remove add x motions )

--- src/sections/Brands.jsx
+++ src/sections/Brands.jsx
@@ -7,64 +7,64 @@ const Brands = () => {
       <div className="max-w-7xl mx-auto">
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
           <motion.div
-            initial={{ opacity: 0, y: 50, x: -50, rotate: 5 }}
-            whileInView={{ opacity: 1, y: 0, x: 0, rotate: 0 }}
+            initial={{ opacity: 0, y: 50, rotate: 5 }}
+            whileInView={{ opacity: 1, y: 0, rotate: 0 }}
             transition={{ duration: 0.5, delay: 0 }}
             className=""
           >
             <BrandCard />
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: 50, x: -50, rotate: 5 }}
-            whileInView={{ opacity: 1, y: 0, x: 0, rotate: 0 }}
+            initial={{ opacity: 0, y: 50, rotate: 5 }}
+            whileInView={{ opacity: 1, y: 0, rotate: 0 }}
             transition={{ duration: 0.5, delay: 0.1 }}
             className=""
           >
             <BrandCard />
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: 50, x: -50, rotate: 5 }}
-            whileInView={{ opacity: 1, y: 0, x: 0, rotate: 0 }}
+            initial={{ opacity: 0, y: 50, rotate: 5 }}
+            whileInView={{ opacity: 1, y: 0, rotate: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
             className=""
           >
             <BrandCard />
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: 50, x: -50, rotate: 5 }}
-            whileInView={{ opacity: 1, y: 0, x: 0, rotate: 0 }}
+            initial={{ opacity: 0, y: 50, rotate: 5 }}
+            whileInView={{ opacity: 1, y: 0, rotate: 0 }}
             transition={{ duration: 0.5, delay: 0.3 }}
             className=""
           >
             <BrandCard />
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: 50, x: 50, rotate: -5 }}
-            whileInView={{ opacity: 1, y: 0, x: 0, rotate: 0 }}
+            initial={{ opacity: 0, y: 50, rotate: -5 }}
+            whileInView={{ opacity: 1, y: 0, rotate: 0 }}
             transition={{ duration: 0.5, delay: 0.3 }}
             className=""
           >
             <BrandCard />
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: 50, x: 50, rotate: -5 }}
-            whileInView={{ opacity: 1, y: 0, x: 0, rotate: 0 }}
+            initial={{ opacity: 0, y: 50, rotate: -5 }}
+            whileInView={{ opacity: 1, y: 0, rotate: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
             className=""
           >
             <BrandCard />
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: 50, x: 50, rotate: -5 }}
-            whileInView={{ opacity: 1, y: 0, x: 0, rotate: 0 }}
+            initial={{ opacity: 0, y: 50, rotate: -5 }}
+            whileInView={{ opacity: 1, y: 0, rotate: 0 }}
             transition={{ duration: 0.5, delay: 0.1 }}
             className=""
           >
             <BrandCard />
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: 50, x: 50, rotate: -5 }}
-            whileInView={{ opacity: 1, y: 0, x: 0, rotate: 0 }}
+            initial={{ opacity: 0, y: 50, rotate: -5 }}
+            whileInView={{ opacity: 1, y: 0, rotate: 0 }}
             transition={{ duration: 0.5, delay: 0 }}
             className=""
           >
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,28 +6,28 @@ const Hero = () => {
       <div className="max-w-7xl mb-10 mt-36 mx-auto m-4">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
           <motion.div
-            initial={{ opacity: 0, x: -100 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
             className="order-2 md:order-1 flex flex-col p-2 bg-neutral-100 rounded-xl"
           >
             <div className="grid grid-cols-1 mb-2 gap-2 md:grid-cols-2">
               <motion.div
-                initial={{ opacity: 0, x: -50 }}
-                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1, delay: 0.4 }}
                 className="bg-yellow-300 py-32 rounded-lg"
               ></motion.div>
               <motion.div
-                initial={{ opacity: 0, x: -50 }}
-                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1, delay: 0.2 }}
                 className="bg-green-500 py-32 rounded-lg"
               ></motion.div>
             </div>
             <motion.div
-              initial={{ opacity: 0, x: -50 }}
-              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, y: -50 }}
+              whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.6 }}
               className="bg-red-400 py-32 rounded-lg"
             >
@@ -35,8 +35,8 @@ const Hero = () => {
             </motion.div>
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, x: 100 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
             className="order-1 md:order-2 py-64 bg-blue-600 rounded-xl"
           >
