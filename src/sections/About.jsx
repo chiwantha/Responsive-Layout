5c16c08a4a25e57e22ee2c56de1709b37daef1d0
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-09-23 19:58:23 +0530

    finished the about section with animations with framer motion too

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,25 +1,47 @@
+import { motion } from "framer-motion";
+
 const About = () => {
   return (
-    <div className="mx-4 pb-20">
-      <div className="bg-white max-w-7xl mx-auto mt-3 space-y-2 rounded-xl p-2">
-        <div className="bg-red-500 py-4 rounded-lg">
+    <div className="mx-4 pb-14">
+      <div className="bg-blue-300/20 max-w-7xl mx-auto mt-3 space-y-2 rounded-xl p-2">
+        <motion.div
+          initial={{ opacity: 0, y: -30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="bg-blue-500 py-4 rounded-lg"
+        >
           <h1 className="text-center text-4xl ktext">TITLE</h1>
-        </div>
+        </motion.div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
           <div className="order-1 md:order-0 space-y-2">
-            <div className="bg-red-600 py-44 rounded-lg">
+            <motion.div
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+              className="bg-red-600 py-44 rounded-lg"
+            >
               <h1 className="text-center text-4xl ktext">TEXT</h1>
-            </div>
+            </motion.div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-              <div className="bg-red-300 py-8 rounded-lg">
+              <motion.div
+                initial={{ opacity: 0, y: 40 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, delay: 0.1 }}
+                className="bg-orange-500 py-8 rounded-lg"
+              >
                 <h1 className="text-center text-4xl ktext">LOGO</h1>
-              </div>
-              <div className="bg-red-400 py-8 rounded-lg">
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: 40 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6 }}
+                className="bg-red-400 py-8 rounded-lg"
+              >
                 <h1 className="text-center text-4xl ktext">BTN</h1>
-              </div>
+              </motion.div>
             </div>
           </div>
-          <div className="order-0 md:order-1 bg-red-800 py-56 rounded-lg">
+          <div className="order-0 md:order-1 bg-green-500 py-56 rounded-lg">
             <h1 className="text-center text-4xl ktext">IMAGE</h1>
           </div>
         </div>
